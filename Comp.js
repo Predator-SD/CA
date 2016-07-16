177adb4150656c690fdb01b0bcac313310c91d5a
SD <zswdzly@outlook.com>
2016-07-16 21:11:46 +0800

    先定义下类

---
+++ Comp.js
@@ -0,0 +1,10 @@
+var Component=function(name,type,voltage,current,resistence){
+  this.name=name;
+  this.type=type;
+  this.voltage=voltage;
+  this.current=current;
+  this.resistence=resistence;
+};
+var Loop=function(components){
+  this.components=components;
+};
